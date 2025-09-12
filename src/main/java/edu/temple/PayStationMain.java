fe0953b521b100ae8ef5482727e5117ba00199a5
Yong Huang <yong48436@gmail.com>
2025-09-12 11:37:21 -0400

    Adding menu for user input

--- src/main/java/edu/temple/PayStationMain.java
+++ src/main/java/edu/temple/PayStationMain.java
@@ -1,7 +1,17 @@
 package edu.temple;
+import java.util.Scanner;
 
 public class PayStationMain {
     public static void main (String[] args){
-        
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Menu:");
+        System.out.println("1. Deposit coins");
+        System.out.println("2. Display");
+        System.out.println("3. Buy Ticket");
+        System.out.println("4. Cancel");
+        System.out.println("0. Admin interface");
+
+        int input = userInput.nextInt();
+        System.out.println("You entered: " + input);
     }
 }
