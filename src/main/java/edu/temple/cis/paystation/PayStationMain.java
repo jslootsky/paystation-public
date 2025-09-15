d1b272d125833aa0a93f85f0b91bd293f87f2a9a
Joshua Slootsky <joshuaslootsky@gmail.com>
2025-09-15 13:17:20 -0400

    modified deposit option to echo input and return everything entered after an exit. formatting changes

--- src/main/java/edu/temple/cis/paystation/PayStationMain.java
+++ src/main/java/edu/temple/cis/paystation/PayStationMain.java
@@ -1,4 +1,5 @@
 package edu.temple.cis.paystation;
+import java.util.Map;
 import java.util.Scanner;
 
 public class PayStationMain {
@@ -17,6 +18,7 @@ public class PayStationMain {
             switch (input){
                 case(1):
                     depositCoins();
+                    break; //prevents falling straight to case 4 after case 1 is executed
                 case(4):
                 running = false;
             }
@@ -28,6 +30,7 @@ public class PayStationMain {
         
     /// function that prints the menu
     private static void printMenu(){
+        System.out.println("Hello! Welcome to the PayStation. Please select your choice:");
         System.out.println("Menu:");
         System.out.println("1. Deposit coins");
         System.out.println("2. Display");
@@ -38,20 +41,27 @@ public class PayStationMain {
 
     //functions that carry out menu functions 
     private static void depositCoins() throws IllegalCoinException{
-        System.out.println("Enter -1 to exit");
+        System.out.println("You have selected: Deposit coins. Enter -1 to exit");
         Scanner input = new Scanner(System.in);
+
         int coinInput = input.nextInt();
         while(coinInput != -1){
             try{
                 ps.addPayment(coinInput);
+                System.out.println("You have entered: " + coinInput + ". Enter -1 to exit");
             }catch (Exception e){
                 System.out.println(e.toString());
             }
             coinInput = input.nextInt();
-            
-            System.out.println("Enter -1 to exit");
         }
-        
+
+        //summary
+                Map<Integer, Integer> coins = ps.returnMap();
+        System.out.println("Coins inserted: 5:" + coins.getOrDefault(5, 0) +
+                ", 10:" + coins.getOrDefault(10, 0) +
+                ", 25:" + coins.getOrDefault(25, 0));
+
+        input.close();
     }
 
 }
